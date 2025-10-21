30bc34f46f8ae23cc683655d0116279a04e6aaf6
Sameed Jamal <sameedjamalkhan@gmail.com>
2025-10-21 16:13:01 +0500

    Uploaded Assests, Imported Font and TailwindCSS

---
+++ db-project/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-[200px] h-[200px] bg-[#1a5d3e] flex items-center justify-center text-white text-xl font-medium">
+      CrimeLens.
+    </div>
+  );
+}
+
+export default App;
