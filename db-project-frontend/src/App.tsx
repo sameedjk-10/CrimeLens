b1efb6ed5e6b2439e58b9b65aad07caf3dcea1d4
Abubakar <k230801@nu.edu.pk>
2025-11-17 23:27:12 +0500

    Routing Problem solved

--- db-project-frontend/src/App.tsx
+++ db-project-frontend/src/App.tsx
@@ -1,22 +1,43 @@
 import Dashboard from "./components/Dashboard";
 import AllRecords from "./components/AllRecords";
-import Verification from "./components/Verification";
 import UploadPage from "./pages/UploadPage";
+import { Routes, Route } from "react-router-dom";
+import StatisticsPage from "./pages/StatisticsPage";
+import ReportCrimePage from "./pages/ReportCrimePage";
+import VerificationPage from "./pages/VerificationPage";
+import AllRecordsPage from "./pages/AllRecordsPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import AdminLogin from "./pages/AdminLogin";
+import PoliceLogin from "./pages/PoliceLogin";
+import AddPoliceAgent from "./pages/AddPoliceAgent";
+
 
 function App() {
   return (
     <Routes>
       {/* Default route → show HomePage */}
+      {/* <Route path="/" element={<HomePage />} /> */}
+      <AllRecordsPage version="admin" />
+
+
+      {/*Default route → show HomePage  */}
       <Route path="/" element={<HomePage />} />
+
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dashboard" element={<Dashboard/>} />
       <Route path="/admin-login" element={<AdminLogin />} />
       <Route path="/police-login" element={<PoliceLogin />} />
-      <Route path="/add-police-agent" element={<AddPoliceAgent />} /> */}
+      <Route path="/add-police-agent" element={<AddPoliceAgent />} />
       <Route path="/upload-crimes" element={<UploadPage version="admin" />} />
       <Route path="/upload-crimes-police" element={<UploadPage version="police" />} />
+      <Route path="/verification" element={<VerificationPage version="police"/>} />
+      <Route path="/statistics" element={<StatisticsPage version="police"/>} />
+      <Route path="/report-crime" element={<ReportCrimePage />} />
+      <Route path="/all-records" element={<AllRecords version="admin"/>} />
+      
 
-  </Routes> */
+    </Routes>
 
   );
 }
