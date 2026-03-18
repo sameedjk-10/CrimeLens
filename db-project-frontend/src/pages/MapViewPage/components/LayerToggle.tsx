de14891d26f5a3ca77f124b9e799be2615f3a2ff
Sameed Jamal <sameedjamalkhan@gmail.com>
2026-03-18 08:03:06 +0500

    syntax minor change

--- db-project-frontend/src/pages/MapViewPage/components/LayerToggle.tsx
+++ db-project-frontend/src/pages/MapViewPage/components/LayerToggle.tsx
@@ -6,7 +6,7 @@ const LayerToggle: React.FC = () => {
   const { highlightZoneLayer, setHighlightZoneLayer } = useContext(MapContext);
 
   return (
-    <div className="absolute bottom-4 right-2 sm:bottom-5 sm:right-5 z-[1000]">
+    <div className="absolute bottom-4 right-2 sm:bottom-5 sm:right-5 z-1000">
       <button
         className="px-3 py-1.5 sm:px-6 sm:py-2 font-semibold bg-linear-to-r from-[#145332] to-[#237E54] border-2 border-[#237E54] hover:from-[#145332] hover:to-[#145332] rounded-xl sm:rounded-2xl text-xs sm:text-md text-[#FFFFFF] shadow"
         onClick={() => setHighlightZoneLayer(prev => !prev)}
